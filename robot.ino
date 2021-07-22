661cb19b1af80b10d524af6b3865d96c0b8e6067
Taifobaid <67175109+Taifobaid@users.noreply.github.com>
2021-07-23 01:27:30 +0300

    Add files via upload

---
+++ robot.ino
@@ -0,0 +1,27 @@
+const int trigPin = 3; const int echoPin = 2; long duration, cm;
+
+void setup() {
+  Serial.begin(9600);  pinMode(trigPin, OUTPUT);  pinMode(echoPin, INPUT);
+}
+
+void loop()
+{
+   
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);  
+  digitalWrite(trigPin, HIGH);  
+  delayMicroseconds(10);  
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+ 
+  cm = (duration/2)/29.1;
+  if(cm < 20)
+  {
+    Serial.print("d"); delay(400);
+  }
+  else
+  {
+    Serial.print("n"); delay(400);
+  }  
+}
